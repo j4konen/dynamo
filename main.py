6746b9c57af62ecc9699164f163a895c7c78e8d2
Sami Jakonen <sami.jakonen@iki.fi>
2019-06-03 15:14:29 +0300

    Make send_to_dns send requests

--- main.py
+++ main.py
@@ -3,13 +3,30 @@
 from flask import Flask, render_template
 from flask import request
 import requests
+import os
 
 
 API_URL = "https://api.cloudflare.com/client/v4/zones/"
 
 
-def send_to_dns():
-    print(0)
+def send_to_dns(sub_domain, ip):
+    zone_id = os.getenv('CFZONE')
+    api_key = os.getenv('CFAPI')
+    auth_mail = os.getenv('CFEMAIL')
+
+    target = API_URL + zone_id + "/dns_records"
+    request_json = {"type": "A", "name": sub_domain + ".vey.cool", "content": ip}
+    print(request_json)
+
+    req = requests.post(target,
+                        headers={"X-Auth-Email": auth_mail, "X-Auth-Key": api_key, "Content-Type": "application/json"},
+                        data=request_json
+                        )
+
+    print(req.text)
+
+
+send_to_dns("hello", "8.8.8.8")
 
 
 # Initiate the api app
